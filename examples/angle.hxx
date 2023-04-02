4e41e1a30adb0d2f3da5446aa397f7ef4d02d334
David Browne <smokidoke@gmail.com>
2023-04-02 13:00:30 -0700

    small refactoring tweaks

--- examples/angle.hxx
+++ examples/angle.hxx
@@ -13,7 +13,7 @@ using namespace dsga;
 //
 
 template <bool W1, floating_point_scalar T, std::size_t C, class D1, bool W2, class D2>
-requires ((C > 1u) && (C < 4u))
+requires ((C == 2u) || (C == 3u))
 auto angle_between(const vector_base<W1, T, C, D1> &v1,
 				   const vector_base<W2, T, C, D2> &v2)
 {
