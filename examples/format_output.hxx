dc988ed387817c486182bf880bcfecdfb37ac55e
David Browne <smokidoke@gmail.com>
2023-04-04 19:07:41 -0700

    refactor output example

--- examples/format_output.hxx
+++ examples/format_output.hxx
@@ -4,11 +4,11 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-#include <iostream>
 #include "dsga.hxx"
 
 #if defined(__cpp_lib_format) && defined(_MSC_VER)
 
+#include <iostream>
 #include <format>
 
 //
@@ -223,75 +223,3 @@ inline void test_format_matrix(const dsga::basic_matrix<T, C, R> &m)
 }
 
 #endif
-
-//
-// iostream interface
-//
-
-template <bool Writable, dsga::dimensional_scalar T, std::size_t Count, typename Derived>
-inline std::ostream &operator<<(std::ostream &o, const dsga::vector_base<Writable, T, Count, Derived> &v)
-{
-	o << "{ " << v[0];
-	for (int i = 1; i < v.length(); ++i)
-		o << ", " << v[i];
-	return o << " }";
-}
-
-template <dsga::dimensional_scalar T, std::size_t Size>
-inline std::ostream &operator<<(std::ostream &o, const dsga::basic_vector<T, Size> &v)
-{
-	o << "{ " << v[0];
-	for (int i = 1; i < v.length(); ++i)
-		o << ", " << v[i];
-	return o << " }";
-}
-
-template <dsga::dimensional_scalar T, std::size_t Size, std::size_t Count, std::size_t ...Is>
-inline std::ostream &operator<<(std::ostream &o, const dsga::indexed_vector<T, Size, Count, Is...> &v)
-{
-	o << "{ " << v[0];
-	for (int i = 1; i < v.length(); ++i)
-		o << ", " << v[i];
-	return o << " }";
-}
-
-template <dsga::floating_point_scalar T, std::size_t C, std::size_t R>
-inline std::ostream &operator<<(std::ostream &o, const dsga::basic_matrix<T, C, R> &m)
-{
-	o << "[ " << m[0];
-	for (int i = 1; i < m.length(); ++i)
-		o << ", " << m[i];
-	return o << " ]";
-}
-
-//
-// test functions
-//
-
-template <bool Writable, dsga::dimensional_scalar T, std::size_t Count, typename Derived>
-inline void test_ostream_vector_base(const dsga::vector_base<Writable, T, Count, Derived> &v)
-{
-	// iostream interface
-	std::cout << v << '\n';
-}
-
-template <dsga::dimensional_scalar T, std::size_t Size>
-inline void test_ostream_vector(const dsga::basic_vector<T, Size> &v)
-{
-	// iostream interface
-	std::cout << v << '\n';
-}
-
-template <dsga::dimensional_scalar T, std::size_t Size, std::size_t Count, std::size_t ...Is>
-inline void test_ostream_indexed_vector(const dsga::indexed_vector<T, Size, Count, Is...> &v)
-{
-	// iostream interface
-	std::cout << v << '\n';
-}
-
-template <dsga::floating_point_scalar T, std::size_t C, std::size_t R>
-inline void test_ostream_matrix(const dsga::basic_matrix<T, C, R> &m)
-{
-	// iostream interface
-	std::cout << m << '\n';
-}
---
+++ examples/ostream_output.hxx
@@ -0,0 +1,80 @@
+
+//          Copyright David Browne 2020-2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "dsga.hxx"
+#include <iostream>
+
+//
+// iostream interface
+//
+
+template <bool Writable, dsga::dimensional_scalar T, std::size_t Count, typename Derived>
+inline std::ostream &operator<<(std::ostream &o, const dsga::vector_base<Writable, T, Count, Derived> &v)
+{
+	o << "{ " << v[0];
+	for (int i = 1; i < v.length(); ++i)
+		o << ", " << v[i];
+	return o << " }";
+}
+
+template <dsga::dimensional_scalar T, std::size_t Size>
+inline std::ostream &operator<<(std::ostream &o, const dsga::basic_vector<T, Size> &v)
+{
+	o << "{ " << v[0];
+	for (int i = 1; i < v.length(); ++i)
+		o << ", " << v[i];
+	return o << " }";
+}
+
+template <dsga::dimensional_scalar T, std::size_t Size, std::size_t Count, std::size_t ...Is>
+inline std::ostream &operator<<(std::ostream &o, const dsga::indexed_vector<T, Size, Count, Is...> &v)
+{
+	o << "{ " << v[0];
+	for (int i = 1; i < v.length(); ++i)
+		o << ", " << v[i];
+	return o << " }";
+}
+
+template <dsga::floating_point_scalar T, std::size_t C, std::size_t R>
+inline std::ostream &operator<<(std::ostream &o, const dsga::basic_matrix<T, C, R> &m)
+{
+	o << "[ " << m[0];
+	for (int i = 1; i < m.length(); ++i)
+		o << ", " << m[i];
+	return o << " ]";
+}
+
+//
+// test functions
+//
+
+template <bool Writable, dsga::dimensional_scalar T, std::size_t Count, typename Derived>
+inline void test_ostream_vector_base(const dsga::vector_base<Writable, T, Count, Derived> &v)
+{
+	// iostream interface
+	std::cout << v << '\n';
+}
+
+template <dsga::dimensional_scalar T, std::size_t Size>
+inline void test_ostream_vector(const dsga::basic_vector<T, Size> &v)
+{
+	// iostream interface
+	std::cout << v << '\n';
+}
+
+template <dsga::dimensional_scalar T, std::size_t Size, std::size_t Count, std::size_t ...Is>
+inline void test_ostream_indexed_vector(const dsga::indexed_vector<T, Size, Count, Is...> &v)
+{
+	// iostream interface
+	std::cout << v << '\n';
+}
+
+template <dsga::floating_point_scalar T, std::size_t C, std::size_t R>
+inline void test_ostream_matrix(const dsga::basic_matrix<T, C, R> &m)
+{
+	// iostream interface
+	std::cout << m << '\n';
+}
