84deaa31efbe7ede0eaa78ef13d5e1707c2213af
David Browne <smokidoke@gmail.com>
2023-07-28 08:07:50 -0700

    refactored utility tests

--- tests/utility_test.cxx
+++ tests/utility_test.cxx
@@ -15,13 +15,6 @@ using namespace dsga;
 //#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-template<std::size_t ...Args1, std::size_t ...Args2>
-requires (sizeof...(Args1) == sizeof...(Args2))
-bool operator ==(std::index_sequence<Args1...>, std::index_sequence<Args2...>) noexcept
-{
-	return ((Args1 == Args2) && ...);
-}
-
 TEST_SUITE("test utility support")
 {
 	TEST_CASE("test index ranges")
@@ -29,24 +22,16 @@ TEST_SUITE("test utility support")
 
 		SUBCASE("half-closed")
 		{
-			auto foo1 = dsga::make_index_range<5, 2>();
-			auto bar1 = dsga::make_index_range<2, 5>();
-			auto baz1 = dsga::make_index_range<2, 2>();
-
-			CHECK_UNARY(foo1 == std::index_sequence<5, 4, 3>());
-			CHECK_UNARY(bar1 == std::index_sequence<2, 3, 4>());
-			CHECK_UNARY(baz1 == std::index_sequence<>());
+			CHECK_UNARY(std::same_as<dsga::make_index_range<5, 2>, std::index_sequence<5, 4, 3>>);
+			CHECK_UNARY(std::same_as<dsga::make_index_range<2, 5>, std::index_sequence<2, 3, 4>>);
+			CHECK_UNARY(std::same_as<dsga::make_index_range<2, 2>, std::index_sequence<>>);
 		}
 
 		SUBCASE("closed")
 		{
-			auto foo2 = dsga::make_closed_index_range<5, 2>();
-			auto bar2 = dsga::make_closed_index_range<2, 5>();
-			auto baz2 = dsga::make_closed_index_range<2, 2>();
-
-			CHECK_UNARY(foo2 == std::index_sequence<5, 4, 3, 2>());
-			CHECK_UNARY(bar2 == std::index_sequence<2, 3, 4, 5>());
-			CHECK_UNARY(baz2 == std::index_sequence<2>());
+			CHECK_UNARY(std::same_as<dsga::make_closed_index_range<5, 2>, std::index_sequence<5, 4, 3, 2>>);
+			CHECK_UNARY(std::same_as<dsga::make_closed_index_range<2, 5>, std::index_sequence<2, 3, 4, 5>>);
+			CHECK_UNARY(std::same_as<dsga::make_closed_index_range<2, 2>, std::index_sequence<2>>);
 		}
 	}
 }
