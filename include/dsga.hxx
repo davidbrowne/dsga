9c21c2d788bae4805f5ed20f750a1b4147a7b3f7
David Browne <smokidoke@gmail.com>
2023-08-23 22:05:09 -0700

    #include <climits>

--- include/dsga.hxx
+++ include/dsga.hxx
@@ -16,7 +16,7 @@
 #include <algorithm>				// min()
 #include <numbers>					// pi_v<>, inv_pi_v<>
 #include <version>					// feature test macros
-#include <limits>					// for cxcm
+#include <climits>					// for cxcm
 #include <cmath>					// for cxcm
 #include <numeric>
 #include <cassert>
